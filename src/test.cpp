5c76307a2c7152d246c13bf9578d562e23242d9c
LichengCAO <licheng_cao@126.com>
2025-02-20 21:32:19 +0800

    comment out exception handle to debug

--- src/test.cpp
+++ src/test.cpp
@@ -632,13 +632,14 @@ int main() {
 #include "transparent_app.h"
 int main() {
 	TransparentApp app;
-	try {
-		app.Run();
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		return EXIT_FAILURE;
-	}
+	app.Run(); // easier to debug this way
+	//try {
+	//	app.Run();
+	//}
+	//catch (const std::exception& e) {
+	//	std::cerr << e.what() << std::endl;
+	//	return EXIT_FAILURE;
+	//}
 	system("pause");
 	return EXIT_SUCCESS;
 }
